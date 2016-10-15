1eb7eb56497fc12160baaa9c1cfad7769c0c5718
igroz777 <gilbert.karadja@gmail.com>
2016-10-15 22:42:55 +0100

    Additional test cases provided to check normal functionality of function. All new cases failing as expected.

--- test.js
+++ test.js
@@ -17,4 +17,32 @@ describe("Test that sum of primes is computed properly", function() {
       lib.sumOfPrimes("invalidValue")== -1
       );
   });
+  it("Entering 10 should return 15", function() {
+    assert(
+      lib.sumOfPrimes(10)== 15
+    );
   });
+  it("Entering 20 should return 75", function() {
+    assert(
+      lib.sumOfPrimes(20)== 75
+    );
+  });
+  it("Entering 30 should return 127", function() {
+    assert(
+      lib.sumOfPrimes(30)== 127
+    );
+  });
+  it("Entering 1 should return 0", function() {
+    assert(lib.sumOfPrimes(1)==0);
+  });
+  it("Entering 2 should return 0", function() {
+    assert(
+      lib.sumOfPrimes(2)== 0
+    );
+  });
+  it("Sum of primes from 0 to 0 should be 0", function() {
+    assert(
+      lib.sumOfPrimes(0)== 0
+    );
+  });
+});
